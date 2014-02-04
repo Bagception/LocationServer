9ba6f5b8e4eaa22ef7a55832b49e9cf75d2a8083
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-02-04 15:38:49 +0100

    added bluetooth support

--- src/de/uniulm/bagception/location/service/LocationService.java
+++ src/de/uniulm/bagception/location/service/LocationService.java
@@ -2,32 +2,23 @@ package de.uniulm.bagception.location.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import de.uniulm.bagception.services.attributes.OurLocation;
-
-import android.app.IntentService;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
+import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.ResultReceiver;
-import android.os.SystemClock;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
 
 public class LocationService extends Service{
 	
@@ -36,6 +27,8 @@ public class LocationService extends Service{
 	private LocationManager locationManager;
 	private LocationListener locationListener;
 	private BluetoothAdapter bluetoothAdapter;
+	private BroadcastReceiver mReceiver;
+	private boolean isBTRegistered = false;
 	
 	
 	private ArrayList<Location> storedLocations;
@@ -69,18 +62,27 @@ public class LocationService extends Service{
 		};
 	}
 	
+	
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		unregisterListeners();
+	}
+
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
-		storedLocations.clear();
-		
 		resultReceiver = intent.getParcelableExtra("receiverTag");
 		log("request received!");
+		
+		
 		String requestType = "";
-		log("requesttype...." + intent.getStringExtra(OurLocation.REQUEST_TYPE));
 		if(intent.hasExtra(OurLocation.REQUEST_TYPE))requestType = intent.getStringExtra(OurLocation.REQUEST_TYPE);
 		
-		if(requestType.equals(OurLocation.LOCATION)){
+		
+		if(requestType.equals(OurLocation.GETLOCATION)){
+			storedLocations.clear();
 			log("RequestType: " + intent.getStringExtra(OurLocation.REQUEST_TYPE));
 			
 			// check if device supports bluetooth
@@ -88,6 +90,7 @@ public class LocationService extends Service{
 				log("hasBT");
 				if(bluetoothAdapter.isEnabled()){
 					log("isEnabledBT");
+					searchForBluetoothDevices();
 					// TODO: BT search...
 					// http://developer.android.com/guide/topics/connectivity/bluetooth.html
 				}
@@ -95,48 +98,90 @@ public class LocationService extends Service{
 
 			// check if gps based location search is enabled
 			if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-				log("isEnabledGPS");
 				locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+				log("gps based enabled");
 			}
 			
 			// check if network and cell based location search is enabled
 			if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
 				locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-				log("isEnabledNetwork");
-				WifiManager mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-			    WifiInfo currentWifi = mainWifi.getConnectionInfo();
-			    if(currentWifi != null){
-			    	// TODO: Add MAC type
-			    	// TODO: Check if mac is in db
-			    	// just for testing...
-			    	Location loc = new Location("WLAN");
-			    	loc.setAccuracy(1);
-			    	loc.setAltitude(99);
-			    	loc.setLatitude(99);
-			    	sendBestPositionFromLocations(loc);
-			    }
+				// search for nearby wifi aps and check if their mac match with a location mac
+				searchForWifiAccessPoints();
+				log("network based enabled");
 			}
-			
-			
-			// if nothing is enabled
-//			if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) && !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
-//				// TODO: kein dienst aktiviert... aufforderung senden!
-////				storedLocations.clear();
-//				locationManager.removeUpdates(locationListener);
-//				Bundle b = new Bundle();
-//				b.putString(OurLocation.RESPONSE_TYPE, OurLocation.LOCATION);
-//				b.putString(OurLocation.PROVIDER, OurLocation.NONE);
-//				resultReceiver.send(0, b);
-//				log("sending answer...");
-//			}
-		}
-			
-		if(requestType.equals(OurLocation.RESOLVE_ADDRESS)){
 			
 			
 		}
 		
+		
+		if(requestType.equals(OurLocation.RESOLVE_ADDRESS)){
+			
+		}
+		
 		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	
+	
+	/**
+	 * searches for nearby wifi access points and checks if a detected mac (bssid) matches with locations mac
+	 */
+	public void searchForWifiAccessPoints(){
+		log("searchForWifiAPs");
+		WifiManager mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		
+		IntentFilter i = new IntentFilter();
+	    i.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
+	    registerReceiver(new BroadcastReceiver(){
+	            @Override
+	            public void onReceive(Context context, Intent intent) {
+	                WifiManager mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+	                mWifiManager.getScanResults();
+	                List<ScanResult> scanResults = mWifiManager.getScanResults();
+	                for(ScanResult sr : scanResults){
+	                	// TODO: 
+	                	// 1. check if mac(bssid) is in db
+	                	// 2. get location (lat,lng)
+	                	// 3. call sendBestPositionFromLocations()
+	                	log("Name: " + sr.SSID + " MAC: " + sr.BSSID);
+	                	Location loc = new Location("WIFI");
+	                	loc.setLatitude(99);
+	                	loc.setLongitude(99);
+	                	loc.setAccuracy(1);
+	                	sendBestPositionFromLocations(loc);
+	                }
+	            }
+	        }
+	    ,i);
+	    mainWifi.startScan();
+	}
+	
+	public void searchForBluetoothDevices(){
+		log("searchForBTDevices");
+		// Create a BroadcastReceiver for ACTION_FOUND
+		mReceiver = new BroadcastReceiver() {
+		    public void onReceive(Context context, Intent intent) {
+		        String action = intent.getAction();
+		        // When discovery finds a device
+		        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+		            // Get the BluetoothDevice object from the Intent
+		            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                	// TODO: 
+                	// 1. check if mac(device.getAddress()) is in db
+                	// 2. get location (lat,lng)
+		           Location loc = new Location("BLUETOOTH");
+		           loc.setAccuracy(1);
+		           loc.setLatitude(99);
+		           loc.setLongitude(99);
+		           sendBestPositionFromLocations(loc);
+		        }
+		    }
+		};
+		// Register the BroadcastReceiver
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+		isBTRegistered = true;
 	}
 	
 
@@ -155,11 +200,11 @@ public class LocationService extends Service{
 				bestLocation = l;
 			}
 		}
+		
 		log("bestLoc acc: " + bestLocation.getAccuracy() + " type: " + bestLocation.getProvider());
 		if(bestLocation.getAccuracy() < LOCATION_ACCURACY_THRESHOLD){
 			storedLocations.clear();
-			locationManager.removeUpdates(locationListener);
-			
+			unregisterListeners();
 			double latitude = bestLocation.getLatitude();
 			double longitude = bestLocation.getLongitude();
 			float accuracy = bestLocation.getAccuracy();
@@ -175,9 +220,17 @@ public class LocationService extends Service{
 			b.putString(OurLocation.PROVIDER, provider);
 			
 			resultReceiver.send(0, b);
-			
 			log("sending answer...");
 		}
+	}
+	
+	private void unregisterListeners(){
+		// unregisterBluetooth
+		if(isBTRegistered){
+			unregisterReceiver(mReceiver); 
+			isBTRegistered=false;
+		}
+		locationManager.removeUpdates(locationListener);
 	}
 	
 	private void log(String s){
